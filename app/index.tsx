498704141780a122dd6605c50e0517507ed1adc3
betomoedano <betomoedano@outlook.com>
2025-11-18 11:21:37 -0600

    simplify project

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { ScrollView, View } from "react-native";
+
+import { HelloWave } from "@/components/hello-wave";
+import { ThemedText } from "@/components/themed-text";
+
+const colors = [];
+
+export default function HomeScreen() {
+  return (
+    <ScrollView
+      contentInsetAdjustmentBehavior="automatic"
+      contentContainerStyle={{ padding: 16 }}
+    >
+      <View style={{ flexDirection: "row", alignItems: "center", gap: 8 }}>
+        <ThemedText type="title">Welcome!</ThemedText>
+        <HelloWave />
+      </View>
+      <ThemedText>Select a color to start</ThemedText>
+    </ScrollView>
+  );
+}
